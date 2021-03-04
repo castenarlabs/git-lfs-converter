f02b358b8ffc06ccfb7a36d74314c3568d2f7833
Pravin <castenar@gmail.com>
2021-03-04 23:45:06 +0100

    Add Script Duration that will print at end of script after a push

--- main.py
+++ main.py
@@ -6,11 +6,23 @@ import lfs_convert_bfg as lfs_convert
 import logging
 import auth_prep as auth
 from repo_clone_push import clone_dir_rm
+from datetime import datetime
+
+
+# Time Function
+def get_time_utc():
+    global time
+    time = (datetime.utcnow())
+
 
 bare = input("Do you want to Clone a bare repository (Enter Yes or No): ").lower()
 
 while True:
     if bare == "yes":
+        get_time_utc()
+        start_time = time
+        print("\nStart Time", start_time, "\n")
+
         is_bare = True  # Clone Bare Repo
         repo_clone_push.backup_path_construct()  # Constructing Repository Backup Path
         clone_dir_rm()  # Remove Existing Clone Dir before fresh clone
@@ -18,6 +30,12 @@ while True:
         repo_clone_push.backup_repo()  # Backing Up the Repo
         track_lfs.install_lfs()  # Initializing LFS in the repo
         lfs_convert.run_bfg_convert()  # Converting to LFS using "GIT LFS MIGRATE" including a FORCE PUSH option
+
+        get_time_utc()
+        end_time = time
+        print("\nEnd Time", end_time)
+        duration = str(end_time - start_time).split(".")[0]
+        print("Total Script Time :", duration)
         break
     elif bare == "no":
         is_bare = False
