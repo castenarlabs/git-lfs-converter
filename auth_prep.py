a91a0629bb628b3ae921db758383fa43eab85b45
Pravin <castenar@gmail.com>
2021-06-07 23:04:38 +0200

    Change 'variable_check()' to ensure variable 'app_password' and 'username' is checked only if 'repo_url' is a HTTPS URL. Don't check 'username' and 'app_password' if it's SSH

--- auth_prep.py
+++ auth_prep.py
@@ -36,34 +36,39 @@ def variable_check():
         print("\n\u274c  Variable 'repo_path' not defined in .env file")
         print("\nPlease ensure to define the 'repo_path' variable")
         exit(1)
-    try:
-        uname = decouple.config('username')
-        if len(uname) <= 0:
-            print("\u274c  'username' is empty! Please ensure variable is defined correctly")
-            exit(1)
-        print(u'\u2714 "username" variable is defined')
-    except decouple.UndefinedValueError as username_error:
-        print("\n\u274c  Variable 'username' not defined in .env file")
-        print("\nPlease ensure to define the 'username' variable")
-        exit(1)
-    try:
-        pwd = decouple.config('app_password')
-        if len(pwd) <= 0:
-            print("\u274c  'app_password' is empty! Please ensure variable is defined correctly")
-            exit(1)
-        print(u'\u2714 "app_password" variable is defined')
-    except decouple.UndefinedValueError as app_password_error:
-        print("\n\u274c  Variable 'app_password' not defined in .env file")
-        print("\nPlease ensure to define the 'app_password' variable")
-        exit(1)
 
-    if "http" in repo_url.lower():
-        parsed_url = urlparse(repo_url)
-        creds = uname + ":" + pwd
-        full_repo_url = f'{parsed_url.scheme}://{creds}@{parsed_url.netloc}{parsed_url.path}'
-    # If above statement is false, assume the 'repo_url' as SSH URL
-    else:
-        full_repo_url = repo_url
+    try:
+        if repo_url.startswith(("http://", "https://"), 0):
+            try:
+                uname = decouple.config('username')
+                if len(uname) <= 0:
+                    print("\u274c  'username' is empty! Please ensure variable is defined correctly")
+                    exit(1)
+                print(u'\u2714 "username" variable is defined')
+            except decouple.UndefinedValueError as username_error:
+                print("\n\u274c  Variable 'username' not defined in .env file")
+                print("\nPlease ensure to define the 'username' variable")
+                exit(1)
+            try:
+                pwd = decouple.config('app_password')
+                if len(pwd) <= 0:
+                    print("\u274c  'app_password' is empty! Please ensure variable is defined correctly")
+                    exit(1)
+                print(u'\u2714 "app_password" variable is defined')
+            except decouple.UndefinedValueError as app_password_error:
+                print("\n\u274c  Variable 'app_password' not defined in .env file")
+                print("\nPlease ensure to define the 'app_password' variable")
+                exit(1)
+
+       # if "http" in repo_url.lower():
+            parsed_url = urlparse(repo_url)
+            creds = uname + ":" + pwd
+            full_repo_url = f'{parsed_url.scheme}://{creds}@{parsed_url.netloc}{parsed_url.path}'
+            # If above statement is false, assume the 'repo_url' as SSH URL
+        else:
+            full_repo_url = repo_url
+    except:
+        exit(1)
 
     try:
         patterns = decouple.config('patterns')
@@ -74,35 +79,6 @@ def variable_check():
     except decouple.UndefinedValueError as patterns_error:
         print("\n\u274c  Variable 'patterns' not defined in .env file")
         print("\nPlease ensure to define the 'patterns' variable")
-        exit(1)
-
-    try:
-        if decouple.config('Switch_Branch') == 'yes':
-            switch_branch = decouple.config('Switch_Branch')
-            print(u'\u2714 "Switch_Branch" variable is defined')
-            try:
-                branch_name = decouple.config('branch_name')
-                print(u'\u2714 "branch_name" variable is defined')
-                # print(branch_name)
-
-                if len(branch_name) <= 0:
-                    print("\u274c  You stated that you want to Switch Branch, but branch_name variable is empty")
-                    exit(1)
-
-            except decouple.UndefinedValueError as branch_name_error:
-                print("\n\u274c  Variable 'branch_name' not defined in .env file")
-                print("Please ensure to define the 'branch_name' variable")
-                exit(1)
-        else:
-            switch_branch = decouple.config('Switch_Branch')
-            if len(switch_branch) < 2:
-                print("\u274c  'Switch_Branch' is incorrect! State 'yes' or 'no'")
-                exit(1)
-            print(u'\u2714 "Switch_Branch" variable is defined')
-            # print(switch_branch)
-    except decouple.UndefinedValueError as branch_name_error:
-        print("\n\u274c  Variable 'Switch_Branch' not defined in .env file")
-        print("\nPlease ensure to define the 'Switch_Branch' variable")
         exit(1)
 
     print(u'\u2705 All Environments are defined')
